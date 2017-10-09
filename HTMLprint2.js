935d9e234174538131fd6395c4087bc8b6d2a9fa
Darcy McLean <darc.mclean@gmail.com>
2017-10-09 15:33:20 +0000

    LHL w2d1 https exercise step 2

---
+++ HTMLprint2.js
@@ -0,0 +1,46 @@
+/* 20171009 DM - LHL w2d1
+
+  Step 2 - Printing with Buffered Strings
+
+*/
+// Include built-in module for http stuff.
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  // String to build the output from each chunk.
+  var strMsg = ""
+
+  /* Add your code here */
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      //console.log('Chunk Received. Length:', data.length);
+      // Console log each chunk of data.
+      // console.log(data + "\n");
+      strMsg += data + "\n";
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(strMsg);
+    });
+
+  });
+};
+
+// Tester:
+getAndPrintHTML();
