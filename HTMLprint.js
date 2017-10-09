b707a2839f453c91426faeb13879dd77a4d5e467
Darcy McLean <darc.mclean@gmail.com>
2017-10-09 15:24:59 +0000

    LHL w2d1 https exercise step 1

---
+++ HTMLprint.js
@@ -0,0 +1,40 @@
+/* 20171009 DM - LHL w2d1
+
+
+*/
+// Include built-in module for http stuff.
+var https = require('https');
+
+// Step 1 - Printing in Chunks
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      //console.log('Chunk Received. Length:', data.length);
+      // Console log each chunk of data.
+      console.log(data + "\n");
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+
+getAndPrintHTMLChunks();
